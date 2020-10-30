2df617eb40bba347cf1c8358f3b7c0eb43b84d90
ศิวัช โพธาราม <souppy@MacBook-Pro-3.local>
2020-10-31 00:15:00 +0700

    start

---
+++ src/main.js
@@ -0,0 +1,52 @@
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+
+// initial and connect database
+const init = mysql.createConnection(
+    {
+        host: "208.109.8.4",
+        user: "developer",
+        password: "chaluideveloper",
+        database: "education"
+    }
+);
+init.connect(function(error) {
+    if (error) throw error;
+    console.log("connected");
+});
+
+// agree body type request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// router basic
+app.post("/basic", function(req, res) {
+    let response = { query: null, code: 200, value: [] };
+    if (req.body.animal) {
+        if (req.body.animal == "cat") {
+            response.value.push({ animals: "cat", people: "soup" });
+        }
+    }
+    response.value.push({ animals: "dog", people: "max" });
+    res.json(response);
+});
+
+// router advance
+app.post("/advance", function(req, res) {
+    let response = { query: null, code: 200, value: [] };
+    let condition = "";
+    if ( req.body.nickname ) {
+        condition = " WHERE nickname = '" + req.body.nickname + "'";
+    }
+    let sql = "SELECT * FROM students" + condition;
+    init.query(sql, function(error ,result) {
+        if (error) throw error;
+        response.value = result;
+        res.json(response);
+    });    
+});
+
+// start server
+app.listen(10000, () => { "listen port 10000"});
